381e7a063e0580a638bf4173be00e55355271701
PravinRaj Selvaraj <pravinraj.selvaraj@kripya.com>
2023-11-01 02:08:41 +0530

    bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Todo from "./components/Todo";
 
 const App = () =>{
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
+    
     return (
         <>
             <Header />
