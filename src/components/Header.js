219a70bd8f4f0addeab09886953d57259367ad8c
PravinRaj Selvaraj <pravinraj.selvaraj@kripya.com>
2023-11-01 01:14:17 +0530

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = ()=> {
+
+    return (
+        <header className="container-fluid p-3">
+            <h3>ToDo List</h3>
+        </header>
+    );
+}
+
+export default  Header
